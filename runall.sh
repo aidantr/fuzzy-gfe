ee97e02b3f236d339ff9908f2639cb664d16cbc1
Aidan Toner-Rodgers <45545596+aidantr@users.noreply.github.com>
2022-09-06 15:58:55 -0400

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Runall for "A Fuzzy CLustering Approach to Estimating Grouped Fixed-Effects" by Lewis, Melcangi, Pilossoph, and Toner-Rodgers
-# Runs Matlab files to produce all tables and figures in the main text and appendix.
+# Runall for "A Fuzzy Clustering Approach to Estimating Grouped Fixed-Effects" by Lewis, Melcangi, Pilossoph, and Toner-Rodgers (2022)
+# Runs Matlab files to produce all tables and figures in the main text and appendix
 
 # set main directory
 cd <put path here>
@@ -13,5 +13,5 @@ matlab -batch "run('code/simulate_panel.m')"
 matlab -batch "run('code/table1.m')"
 matlab -batch "run('code/fig3.m')"
 matlab -batch "run('code/tableB1.m')"
-matlab -batch "run('code/tableB2.m')"
+
 
