5133beb295826c609060346bb6cddc2da651dd2e
Aidan Toner-Rodgers <45545596+aidantr@users.noreply.github.com>
2022-09-15 17:27:57 -0400

    Update weights.m

--- code/functions/weights.m
+++ code/functions/weights.m
@@ -1,10 +1,11 @@
+%calculates group weights for FCR
+
 function weight = weights(alpha,zeta,y,t,Z,G,m)
 
 T=size(t,2);
 N = size(y,1)/T; 
 
 amat=[reshape(alpha,[G,T]) repmat(zeta',G,1)];
-%amat = [reshape(fe,[T,group])' repmat(td',group,1) repmat(zeta',group,1)];
 del  = y-[t Z]*amat';
 del2 = mat2cell(del,repmat(T,size(y,1)/T,1)',[G]);
 e = permute(cat(3,del2{:}),[3,1,2]);
